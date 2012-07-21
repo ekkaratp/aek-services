cc2988b1ce6b7fe662b575954c2f24761e88774d
Ekkarat <ekkarat.p@gmail.com>
2012-07-21 10:42:12 +0700

    DatastoreService Feature

--- src/com/ekkaratp/aekservices/AppUpdateInfo.java
+++ src/com/ekkaratp/aekservices/AppUpdateInfo.java
@@ -1,21 +1,43 @@
 package com.ekkaratp.aekservices;
 
+import java.util.List;
+
 import javax.xml.bind.annotation.XmlElement;
 import javax.xml.bind.annotation.XmlRootElement;
+
+import com.google.appengine.api.datastore.DatastoreService;
+import com.google.appengine.api.datastore.DatastoreServiceFactory;
+import com.google.appengine.api.datastore.Entity;
+import com.google.appengine.api.datastore.FetchOptions;
+import com.google.appengine.api.datastore.Query;
 
 
 
 @XmlRootElement(name = "appupdateinfo")
 public class AppUpdateInfo {
 
+	private String _version = "";
+	private String _url = "";
+	
+	public AppUpdateInfo() {
+		DatastoreService datastore = DatastoreServiceFactory.getDatastoreService();
+		Query query = new Query("AppUpdateInfo");
+		List<Entity> results = datastore.prepare(query).asList(FetchOptions.Builder.withDefaults());
+		if (results != null && results.size() > 0) {
+			_version = results.get(0).getProperty("version").toString();
+			_url = results.get(0).getProperty("url").toString();
+		}
+		
+	}
+	
 	@XmlElement(name = "version")
 	public String getVesion() {
-		return "1.2";
+		return _version;
 	}
 	
 	@XmlElement(name = "url")	
 	public String getAppUrl() {
-		return "http://dl.dropbox.com/u/23458376/TestUpdate.apk";
+		return _url;
 	}
 	
 }
--- src/com/ekkaratp/aekservices/MobileService.java
+++ src/com/ekkaratp/aekservices/MobileService.java
@@ -3,8 +3,8 @@ package com.ekkaratp.aekservices;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
+import javax.ws.rs.QueryParam;
 import javax.ws.rs.core.MediaType;
-
 
 @Path("/")
 public class MobileService {
@@ -17,4 +17,26 @@ public class MobileService {
 		return app;
 	}
 	
+	@GET
+	@Produces(MediaType.APPLICATION_JSON)
+	@Path("/adddata")
+	public String addData() {
+		boolean ret = false;
+		
+//		DatastoreService  datastore = DatastoreServiceFactory.getDatastoreService();
+//		Entity app = new Entity("AppUpdateInfo", "AppUpdateInfo");
+//		app.setProperty("version", "1.2");
+//		app.setProperty("url", "http://dl.dropbox.com/u/23458376/TestUpdate.apk");
+//		datastore.put(app);
+		ret = true;
+		return String.valueOf(ret);
+		
+	}
+	
+	@GET
+	@Produces(MediaType.APPLICATION_JSON)
+	@Path("/hello")
+	public String hello(@QueryParam("name") String name) {
+		return String.format("Hello %s", name);
+	}
 }
