a48550c6f1892456df7c2abdb9436180cd21a08f
Ekkarat <ekkarat.p@gmail.com>
2012-08-10 10:39:56 +0700

    Add register function

--- src/com/ekkaratp/aekservices/MobileService.java
+++ src/com/ekkaratp/aekservices/MobileService.java
@@ -1,10 +1,17 @@
 package com.ekkaratp.aekservices;
 
+import javax.ws.rs.FormParam;
 import javax.ws.rs.GET;
+import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.QueryParam;
 import javax.ws.rs.core.MediaType;
+
+import com.google.appengine.api.datastore.DatastoreService;
+import com.google.appengine.api.datastore.DatastoreServiceFactory;
+import com.google.appengine.api.datastore.Entity;
 
 @Path("/")
 public class MobileService {
@@ -39,4 +46,29 @@ public class MobileService {
 	public String hello(@QueryParam("name") String name) {
 		return String.format("Hello %s", name);
 	}
+
+	@GET
+	@Produces(MediaType.APPLICATION_JSON)
+	@Path("/register")	
+	public String register(@QueryParam("id") String id,@QueryParam("phone")  String phone) {
+		int ret = 1;
+		if (id != null && phone != null) {
+			if (id.length() > 0 && phone.length() > 0) {
+				try {
+					DatastoreService  datastore = DatastoreServiceFactory.getDatastoreService();
+					Entity app = new Entity("TUCRegister", "TUCRegister");
+					app.setProperty("id", id);
+					app.setProperty("phone", phone);
+					datastore.put(app);				
+					ret = 0;
+					
+				} catch (Exception ex) {
+					
+				}
+			}
+		}
+		return String.valueOf(ret);
+	}
+	
+	
 }
